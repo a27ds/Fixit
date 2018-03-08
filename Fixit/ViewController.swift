9d5cb9e71ee791986892d50b4fa0a2744cf6d833
a27 <andy27ds@mac.com>
2018-03-08 11:43:12 +0100

    First view controller fixed, toolbar and toolbar buttons added, and a round button over the toolbar

--- Fixit/ViewController.swift
+++ Fixit/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        button.layer.cornerRadius = 40
         // Do any additional setup after loading the view, typically from a nib.
     }
 
